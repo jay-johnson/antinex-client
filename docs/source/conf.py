72d98ce1d80e81521954816620a6bfae83207eb2
jay-johnson <jay.p.h.johnson@gmail.com>
2018-04-01 14:28:31 -0700

    removing from conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,6 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {}
 html_theme_options = {
     'canonical_url': '',
-    'analytics_id': '',
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
