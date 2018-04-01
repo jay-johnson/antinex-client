fc43716211cd971e7415f7fcaeff0f3126f50c2c
jay-johnson <jay.p.h.johnson@gmail.com>
2018-04-01 13:20:58 -0700

    trying this extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.intersphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
