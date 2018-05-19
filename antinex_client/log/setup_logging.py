cbcbbdceca9eece376c333f4f42caf22027d5050
jay-johnson <jay.p.h.johnson@gmail.com>
2018-05-19 12:56:46 -0700

    new support for using a shared logger within the container

--- antinex_client/log/setup_logging.py
+++ antinex_client/log/setup_logging.py
@@ -3,16 +3,23 @@ import json
 import logging.config
 
 
-def setup_logging(default_level=logging.INFO,
-                  default_path="{}/logging.json".format(
-                      os.getenv(
-                          "LOG_DIR",
-                          os.path.dirname(os.path.realpath(__file__)))),
-                  env_key="LOG_CFG",
-                  config_name=None):
+def setup_logging(
+        default_level=logging.INFO,
+        default_path="{}/logging.json".format(
+            os.getenv(
+                "LOG_DIR",
+                os.path.dirname(os.path.realpath(__file__)))),
+        env_key="LOG_CFG",
+        config_name=None):
+    """setup_logging
 
-    """
     Setup logging configuration
+
+    :param default_level: level to log
+    :param default_path: path to config (optional)
+    :param env_key: path to config in this env var
+    :param config_name: filename for config
+
     """
     path = default_path
     file_name = default_path.split("/")[-1]
@@ -105,18 +112,55 @@ def build_colorized_logger(
     :param log_level: level to log
     :param log_config_path: path to log config file
     """
-    use_config = ("./log/{}").format(
-                    "{}".format(
-                        config))
+
+    override_config = os.getenv(
+        "SHARED_LOG_CFG",
+        None)
+    debug_log_config = os.getenv(
+        "DEBUG_SHARED_LOG_CFG",
+        None)
+    if override_config:
+        if debug_log_config:
+            print((
+                "creating logger config env var: "
+                "SHARED_LOG_CFG={}".format(
+                    override_config)))
+        if os.path.exists(override_config):
+            setup_logging(
+                default_level=log_level,
+                default_path=override_config)
+            return logging.getLogger(name)
+        if debug_log_config:
+            print((
+                "Failed to find log config using env var: "
+                "SHARED_LOG_CFG={}".format(
+                    override_config)))
+    else:
+        if debug_log_config:
+            print((
+                "Not using shared logging env var: "
+                "SHARED_LOG_CFG={}".format(
+                    override_config)))
+    # allow a shared log config across all components
+
+    use_config = ("{}").format(
+        config)
+
     if not os.path.exists(use_config):
-        use_config = log_config_path
-        if not os.path.exists(use_config):
-            use_config = ("./antinex_client/log/{}").format(
+        use_config = ("./antinex_client/log/{}").format(
                             config)
+        if not os.path.exists(use_config):
+            use_config = log_config_path
             if not os.path.exists(use_config):
-                use_config = ("./antinex_client/log/{}").format(
+                use_config = ("./log/{}").format(
+                            config)
+                if not os.path.exists(use_config):
+                    use_config = ("./antinex_client/log/{}").format(
                                 "logging.json")
-    # find the log processing
+                # find the last log config backup from the base of the repo
+            # find the log config from the defaults with the env LOG_CFG
+        # find the log config from the base of the repo
+    # find the log config by the given path
 
     setup_logging(
         default_level=log_level,
